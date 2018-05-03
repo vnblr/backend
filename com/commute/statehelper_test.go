d19624004f9b2b0a49f134480499dbfcec7415c8
vnblr <vikram.nayak.p@gmail.com>
2018-05-03 14:29:26 +0530

    tests running now but take 45secs post locking changes

--- com/commute/statehelper_test.go
+++ com/commute/statehelper_test.go
@@ -34,28 +34,30 @@ func TestUpdateAuthError(t *testing.T) {
 
 	//Not logged in user
 	token1 := newToken("newuser1")
-	retStr := updateState("token3", 7.1, 10.2, token1, RIDER_STATE)        //wrong user
-	retStr2 := updateState("token1", 7.1, 10.2, "wrongtoken", RIDER_STATE) //wrong token
+	_, err := updateState("token3", 7.1, 10.2, token1, RIDER_STATE, "", EVENT_HEARTBEAT)        //wrong user
+	_, err2 := updateState("token1", 7.1, 10.2, "wrongtoken", RIDER_STATE, "", EVENT_HEARTBEAT) //wrong token
 
-	if strings.Contains(retStr, "Authentication error") != true ||
-		strings.Contains(retStr2, "Authentication error") != true {
-		t.Errorf("Auth errors were not returned. retStr:", retStr, " retStr2:", retStr2)
+	if strings.Contains(err.Error(), "Authentication error") != true ||
+		strings.Contains(err2.Error(), "Authentication error") != true {
+		t.Errorf("Auth errors were not returned. retStr:", err.Error(), " retStr2:", err.Error())
 	}
 }
 
 func TestUpdateExample(t *testing.T) {
 	Initialize()
-	token1 := newToken("newuser1")
-	if countLoggedInUsers() != 1 || countStateUsers() != 0 { //fixme
+
+	//Lets simulate a login event first.
+	token1, _ := updateState("newuser1", 7.1, 10.2, "", RIDER_STATE, "", EVENT_LOGIN)
+	if countLoggedInUsers() != 1 || countStateUsers() != 1 { //fixme
 		t.Errorf("Count mismatch in DS1. LoggedIn:", countLoggedInUsers(), " in DS:", countStateUsers())
 	}
 	fmt.Println("TestUpdateExample here....remove. cnt:", countStateUsers()) //fixme
-	r1 := updateState("newuser1", 7.1, 10.2, token1, RIDER_STATE)
+	r1, _ := updateState("newuser1", 7.1, 10.2, token1, RIDER_STATE, "", EVENT_HEARTBEAT)
 	if countLoggedInUsers() != 1 || countStateUsers() != 1 {
 		t.Errorf("Count mismatch in DS2. LoggedIn:", countLoggedInUsers(), " in DS:", countStateUsers(), " ret:", r1)
 	}
 	//Another update
-	r2 := updateState("newuser1", 7.2, 10.3, token1, RIDER_STATE)
+	r2, _ := updateState("newuser1", 7.2, 10.3, token1, RIDER_STATE, "", EVENT_HEARTBEAT)
 	if countLoggedInUsers() != 1 || countStateUsers() != 1 {
 		t.Errorf("Count mismatch in DS3. LoggedIn:", countLoggedInUsers(), " in DS:", countStateUsers(), " ret:", r2)
 	}
@@ -76,19 +78,21 @@ func TestUpdateExample(t *testing.T) {
 func createAndUpdateUser(userName string, t *testing.T) {
 	defer wg.Done()
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	token := newToken(userName)
+
+	//Simulate a login first..
+	token, _ := updateState(userName, 7.1, 10.2, "", RIDER_STATE, "", EVENT_LOGIN)
 
 	//Fire some random updates
 	retVal := ""
 	for i := 1; i < 1000; i++ {
-		retVal = updateState(userName, 1.2, 2.2, token, RIDER_STATE)
+		retVal, _ = updateState(userName, 1.2, 2.2, token, RIDER_STATE, "", EVENT_HEARTBEAT)
 		//Let parallalism kick in
 		time.Sleep(1 * time.Millisecond)
 	}
 	//Final update
 	lat := r.Float64()
 	lng := r.Float64()
-	retVal = updateState(userName, lat, lng, token, RIDER_STATE)
+	retVal, _ = updateState(userName, lat, lng, token, RIDER_STATE, "", EVENT_HEARTBEAT)
 
 	time.Sleep(100 * time.Millisecond)
 	currState := getCurrentState(userName)
@@ -134,14 +138,12 @@ func TestSearchDrivers(t *testing.T) {
 		{"user7", 150.001, 230.002},
 		{"user8", 150.002, 230.001},
 	}
-	//First update the DS
+	//First update the DS..login these users.
 	for _, c := range cases {
-		token := newToken(c.user)
-		updateState(c.user, c.lat, c.lng, token, DRIVER_STATE)
+		_, _ = updateState(c.user, c.lat, c.lng, "", DRIVER_STATE, "", EVENT_LOGIN)
 	}
 	//testuser:for now dump in any location
-	testToken := newToken("testuser")
-	updateState("testuser", 10.0, 20.0, testToken, RIDER_STATE)
+	testToken, _ := updateState("testuser", 10.0, 20.0, "", RIDER_STATE, "", EVENT_LOGIN)
 
 	var retArr []matchUserDetails
 	var err error
@@ -153,20 +155,20 @@ func TestSearchDrivers(t *testing.T) {
 	}
 
 	//search should result in no nearby users
-	updateState("testuser", 10.0, 20.0, testToken, RIDER_STATE)
+	_, _ = updateState("testuser", 10.0, 20.0, testToken, RIDER_STATE, "", EVENT_HEARTBEAT)
 	retArr, _ = searchMatches("testuser", RIDER_STATE)
 	if len(retArr) != 0 {
 		t.Errorf("Error in searchUsers. len retArr:", len(retArr))
 	}
 
 	//search should return max possible users
-	updateState("testuser", 100.001, 200.003, testToken, RIDER_STATE)
+	_, _ = updateState("testuser", 100.001, 200.003, testToken, RIDER_STATE, "", EVENT_HEARTBEAT)
 	retArr, _ = searchMatches("testuser", RIDER_STATE)
 	if len(retArr) != MAX_MATCHED_USERS {
 		t.Errorf("Error in searchUsers max. len retArr:", len(retArr))
 	}
 	//search should return 2 possible users
-	updateState("testuser", 150.001, 230.003, testToken, RIDER_STATE)
+	_, _ = updateState("testuser", 150.001, 230.003, testToken, RIDER_STATE, "", EVENT_HEARTBEAT)
 	retArr, _ = searchMatches("testuser", RIDER_STATE)
 	if len(retArr) != 2 {
 		t.Errorf("Error in searchUsers max. len retArr:", len(retArr))
@@ -191,14 +193,12 @@ func TestSearchRiders(t *testing.T) {
 		{"user7", 150.001, 230.002},
 		{"user8", 150.002, 230.001},
 	}
-	//First update the DS
+	//First update the DS .. login these users
 	for _, c := range cases {
-		token := newToken(c.user)
-		updateState(c.user, c.lat, c.lng, token, RIDER_STATE)
+		_, _ = updateState(c.user, c.lat, c.lng, "", RIDER_STATE, "", EVENT_LOGIN)
 	}
 	//testuser:for now dump in any location
-	testToken := newToken("testuser")
-	updateState("testuser", 10.0, 20.0, testToken, DRIVER_STATE)
+	testToken, _ := updateState("testuser", 10.0, 20.0, "", DRIVER_STATE, "", EVENT_LOGIN)
 	//Lets fill it with all requests
 	currState := getCurrentState("testuser")
 	for _, c := range cases {
@@ -207,20 +207,20 @@ func TestSearchRiders(t *testing.T) {
 	var retArr []matchUserDetails
 
 	//search should result in no nearby users
-	updateState("testuser", 10.0, 20.0, testToken, DRIVER_STATE)
+	updateState("testuser", 10.0, 20.0, testToken, DRIVER_STATE, "", EVENT_HEARTBEAT)
 	retArr, _ = searchMatches("testuser", DRIVER_STATE)
 	if len(retArr) != 0 {
 		t.Errorf("Error in searchUsers. len retArr:", len(retArr))
 	}
 
 	//search should return max possible users
-	updateState("testuser", 100.001, 200.003, testToken, DRIVER_STATE)
+	updateState("testuser", 100.001, 200.003, testToken, DRIVER_STATE, "", EVENT_HEARTBEAT)
 	retArr, _ = searchMatches("testuser", DRIVER_STATE)
 	if len(retArr) != MAX_MATCHED_USERS {
 		t.Errorf("Error in searchUsers max. len retArr:", len(retArr))
 	}
 	//search should return 2 possible users
-	updateState("testuser", 150.001, 230.003, testToken, DRIVER_STATE)
+	updateState("testuser", 150.001, 230.003, testToken, DRIVER_STATE, "", EVENT_HEARTBEAT)
 	retArr, _ = searchMatches("testuser", DRIVER_STATE)
 	if len(retArr) != 2 {
 		t.Errorf("Error in searchUsers max. len retArr:", len(retArr))
